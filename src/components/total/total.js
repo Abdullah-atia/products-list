d1718092d78f3a9424b5a45543440e2167b16bf2
Abdullah-atia <abdo.atia632158@gmail.com>
2023-10-16 14:34:37 +0300

    add quantity column

--- src/components/total/total.js
+++ src/components/total/total.js
@@ -3,8 +3,22 @@ import React from 'react';
 const Total = (props) => {
     const {items} = props;
     let total = 0
+    // for (let i = 0; i < items.length; i++) {
+    //     let cout = parseFloat(items[i].quantity)
+    //     let price = parseFloat(items[i].price)
+    //     total += price * cout
+    // }
     for (let i = 0; i < items.length; i++) {
-        total += parseFloat(items[i].price)
+        let count = parseFloat(items[i].quantity);
+        let price = parseFloat(items[i].price);
+
+        // Add checks to handle NaN cases
+        if (!isNaN(count) && !isNaN(price)) {
+            total += price * count;
+        } else {
+            // Handle the case where parsing fails
+            console.error(`Error parsing quantity or price at index ${i}`);
+        }
     }
     return (
         <div>
